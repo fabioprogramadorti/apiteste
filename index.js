9f192f4d461fb93666c9a718f18036f091aa7fa2
Fabio Santos <fabioprogramadorti@gmail.com>
2020-09-29 22:22:07 -0300

    modificaçao

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const config = require("config");
 const mongoose = require("mongoose");
 const util = require("./util/index");
 const routes = require("./routes/index");
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
@@ -27,7 +28,23 @@ function main(validate) {
     let mongo = config.get("mongo");
     util.OpenConnection(mongo.server, mongo.database, mongo.user, mongo.pass);
     app.use(express.json());
+    app.use(cors())
 
+    app.use(function (req, res, next) {
+        // Website you wish to allow to connect
+        //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4201');
+        // // // Website you wish to allow to connect
+         res.setHeader('Access-Control-Allow-Origin', '*');
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        // Pass to next layer of middleware
+        next();
+      });
     
     //use users route for api/users
     app.use("/api", routes);
